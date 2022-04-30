2f96b1feeba8518fe29e6c4901ee988c2c7eb94d
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-05-01 00:08:38 +0700

    Create start.sh to start the docker container
    Create a file named start.sh. This is a script to start the container
    with prepared configuration. Only run this script on the host.
    
    [ammarfaizi2: Delete the comment in start.sh.]
    
    Link: https://lore.gnuweeb.org/gwml/20220430170838.19571-5-ammarfaizi2@gnuweeb.org
    
    Reviewed-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Reviewed-by: Hazmi Alfarizqi <hzmi@gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e;
+
+HOSTNAME="gnuweeb.org";
+CONTAINER_NAME="server-a001-ct";
+MYSQL_DATA_DIR="$(readlink -e "./storage/mysql_data")";
+
+mkdir -pv "$MYSQL_DATA_DIR";
+
+CMD="$1";
+
+if [[ "${CMD}" == "force-run" ]]; then
+    docker rm -f "${CONTAINER_NAME}" || true;
+    CMD="run";
+fi;
+
+if [[ "${CMD}" == "run" ]]; then
+    docker run \
+        --name "${CONTAINER_NAME}" \
+        --privileged \
+        --hostname "${HOSTNAME}" \
+        --mount "type=volume,dst=/var/lib/mysql,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=${MYSQL_DATA_DIR}" \
+        --tty \
+        --interactive \
+        --detach \
+        server-a001:latest;
+
+    exit 0;
+fi;
+
+echo "Unknown command ${CMD}!";
+exit 1;
