3cdd0c07736cc9e0f0da06ff9adbf5494cd637dc
Arthur Lapz <rlapz@gnuweeb.org>
2022-07-09 03:47:30 +0000

    mailserver: Add postfix configurations
    Postfix is used for Mail Transfer Agent (MTA) and SMTP server.
    
    I can't explain all of the config options one by one. There are many options
    that I only pick up from random websites, and I'm not sure wether those
    options are necessary or not.
    
    In summary:
    1. Using mysql as mailbox maps.
       * Postfix can maps user home dir using user data that stored in
         mysql database. Incoming mail from an unregistered user will be refused.
    2. Add domain relay for public-inbox (required for gwml).
       * Public inbox script stored in /home/${MAIN_USER}/public_inbox.php
    3. Using ssl/tls for security reason.
    
    Link: https://t.me/GNUWeeb/635644
    Signed-off-by: Arthur Lapz <rlapz@gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- mailserver/setup.sh
+++ mailserver/setup.sh
@@ -7,6 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 # Import mounted docker secret file
 . /run/secrets/config
 
+MAIL_SSL_KEY="/var/keys/ssl/privkey.pem"
+MAIL_SSL_CERT="/var/keys/ssl/fullchain.pem"
+
 
 
 #
@@ -15,3 +18,40 @@ export DEBIAN_FRONTEND=noninteractive
 groupadd -g 5000 vmail
 useradd -g vmail -u 5000 vmail -d /var/mail
 install -v -d -m 0750 -o vmail -g vmail /var/vmail
+
+
+
+#
+# Postfix
+#
+cp -v ./postfix/*.cf /etc/postfix/
+
+postconf myhostname="$MAIL_HOST"
+postconf smtpd_tls_key_file="$MAIL_SSL_KEY"
+postconf smtpd_tls_cert_file="$MAIL_SSL_CERT"
+postconf relay_domains="$MAIL_RELAY_HOST"
+
+# MySQL user maps
+cat << EOF > /etc/postfix/mysql_mailbox_maps.cf
+hosts    = ${DB_HOST}
+user     = ${DB_USER}
+password = ${DB_PASS}
+dbname   = ${DB_NAME}
+query    = SELECT CONCAT(username, "/") FROM users WHERE username="%u"
+EOF
+
+echo "${MAIL_RELAY_HOST} public-inbox: " > /etc/postfix/transport
+echo "${MAIL_RELAY_USER}@${MAIL_HOST} ${MAIL_RELAY_USER}@${MAIL_RELAY_HOST}" > /etc/postfix/virtual_aliases
+
+postmap /etc/postfix/transport /etc/postfix/virtual_aliases
+
+
+# Add public-inbox handler
+cat << EOF >> /etc/postfix/master.cf
+# Public inbox
+public-inbox unix  -      n       n       -       -       pipe
+  flags=FR user=${MAIN_USER} argv=/home/${MAIN_USER}/public_inbox.php
+EOF
+
+chown -v postfix:postfix /etc/postfix/mysql_mailbox_maps.cf
+chmod -v 0600 /etc/postfix/mysql_mailbox_maps.cf
